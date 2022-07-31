549353c7db10bb92c270e02fee3219571b54c8f6
Paulo Carmino <contato@paulocarmino.com>
2022-07-31 02:33:08 -0300

    add search-input component

---
+++ src/components/SearchInput/index.tsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import { TbSearch } from 'react-icons/tb'
+
+const SearchInput = React.forwardRef<HTMLInputElement>(({ ...rest }, ref) => {
+  return (
+    <div className="flex items-center">
+      <div className="w-full">
+        <label htmlFor="search" className="sr-only">
+          Search
+        </label>
+        <div className="relative">
+          <div className="flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none">
+            <TbSearch className="w-5 h-5 text-gray-400" aria-hidden="true" />
+          </div>
+          <input
+            ref={ref}
+            id="search"
+            name="search"
+            className="block py-2 pr-3 pl-10 w-full text-sm placeholder:text-gray-500 focus:text-gray-900 focus:placeholder:text-gray-400 bg-white rounded-md border border-gray-300 focus:outline-none sm:text-sm"
+            placeholder="Search movies..."
+            type="search"
+            {...rest}
+          />
+        </div>
+      </div>
+    </div>
+  )
+})
+
+export default SearchInput
