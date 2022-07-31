35e69c4b07f6a7e2e362075a03b5027e8d7780f0
Paulo Carmino <contato@paulocarmino.com>
2022-07-31 12:15:03 -0300

    increase logo size

--- src/components/Logo/index.tsx
+++ src/components/Logo/index.tsx
@@ -2,8 +2,8 @@ import { FaRegPlayCircle } from 'react-icons/fa'
 
 const Logo = () => (
   <div className='flex items-center'>
-    <div className='text-2xl'>
-      <FaRegPlayCircle data-testid="logo-icon" size={26} className="mr-2 text-yellow-400" />
+    <div>
+      <FaRegPlayCircle data-testid="logo-icon" size={36} className="mr-2 text-yellow-400" />
     </div>
     <h1 className='font-medium text-white'>MovieApp</h1>
   </div>
