7bf5dc1d5dfc5c7e0649966a5dbcf9288eb4fa5b
Paulo Carmino <contato@paulocarmino.com>
2022-08-01 06:14:40 -0300

    refactor _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,33 +1,11 @@
-import axios, { HeadersDefaults } from 'axios';
 import type { AppProps } from 'next/app'
 import { useEffect } from 'react';
-import { uuid } from 'uuidv4';
 
 import '@/styles/globals.css'
 import Template from '@/components/Template'
-
-interface HeaderPropertiesWithBrowserId extends HeadersDefaults {
-  ['browser-id']: string
-}
+import { getBroweserId, setBrowserId } from '@/utils/helpers';
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
-
-  const getBroweserId = () => {
-    if (typeof window !== "undefined") {
-      return localStorage.getItem('movieapp-browser-id')
-    }
-  }
-
-  const setBrowserId = () => {
-    if (typeof window !== "undefined") {
-      const generatedUuid = uuid()
-      localStorage.setItem('movieapp-browser-id', generatedUuid)
-
-      axios.defaults.headers = {
-        ['browser-id']: generatedUuid
-      } as HeaderPropertiesWithBrowserId
-    }
-  }
 
   useEffect(() => {
     const browserId = getBroweserId()
--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -1,3 +1,11 @@
+import { HeadersDefaults } from 'axios';
+import { uuid } from 'uuidv4';
+import axios from './axios';
+
+interface HeaderPropertiesWithBrowserId extends HeadersDefaults {
+  ['browser-id']: string
+}
+
 export const findFavoriteMovies = (favoritesMoviesIds: any, moviesList: any) => {
   const favoritedMovies = favoritesMoviesIds.map((movieId: number) => {
     return moviesList.filter((movie: any) => movie.id === movieId)[0]
@@ -9,5 +17,16 @@ export const findFavoriteMovies = (favoritesMoviesIds: any, moviesList: any) => 
 export const getBroweserId = () => {
   if (typeof window !== "undefined") {
     return localStorage.getItem('movieapp-browser-id')
+  }
+}
+
+export const setBrowserId = () => {
+  if (typeof window !== "undefined") {
+    const generatedUuid = uuid()
+    localStorage.setItem('movieapp-browser-id', generatedUuid)
+
+    axios.defaults.headers = {
+      ['browser-id']: generatedUuid
+    } as HeaderPropertiesWithBrowserId
   }
 }
